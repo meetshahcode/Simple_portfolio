d61b68e659403c881babd4ac98e561bdb2ec279d
Meet Shah <meet.shah@begenuin.com>
2024-01-11 15:46:08 +0530

    final website completed

--- js/script.js
+++ js/script.js
@@ -13,7 +13,6 @@
 const $themeBtn = document.querySelector("[data-theme-btn]");
 const $doc = document.documentElement;
 let isDark = window.matchMedia("(prefers-color-schema:dark)").matches;
-console.log(window)
 
 if (sessionStorage.getItem("theme")) {
     $doc.dataset.theme = sessionStorage.getItem("theme");
@@ -29,3 +28,26 @@ const changeTheme = () => {
 }
 
 $themeBtn.addEventListener("click",changeTheme);
+
+
+/* 
+TAB logic
+*/
+
+const $tabBtn = document.querySelectorAll("[data-tab-btn]");
+let [lastActiveTab] = document.querySelectorAll("[data-tab-content]");
+let [lastActiveTabBtn] = $tabBtn;
+
+$tabBtn.forEach(items => {
+    items.addEventListener("click",function(){
+        lastActiveTab.classList.remove("active");
+        lastActiveTabBtn.classList.remove("active");
+
+        const $tabContent = document.querySelector(`[data-tab-content="${items.dataset.tabBtn}"]`);
+        
+        $tabContent.classList.add("active");
+        this.classList.add("active");
+        lastActiveTab = $tabContent;
+        lastActiveTabBtn = this;
+    });
+});
